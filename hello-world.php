542ab41189c02daaf2bf270c567f80e0e09daea8
Bikal Shrestha <bikshr09@student.wintec.ac.nz>
2018-02-20 00:04:04 +0000

    First Commit

---
+++ hello-world.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+    <head>
+        <title>My Cloud Project</title>
+        <style type="text/css" href="style.css"></style>
+    </head>
+    <header>
+        <div id="header_top">
+       
+        </div>
+        <div id="header_bottom">
+              <div class="logo">
+               <img src="https://learning.wintec.ac.nz/pluginfile.php/1/core_admin/logocompact/0x70/1518486201/wintec-logo.png" alt="logo">
+           </div>
+        </div>
+    </header>
+<h1>My First Project in Cloud</h1>
+<?php
+echo 'Learning cloud 9 is intresting.'
+
+?>
+<footer>
+    <p style="text-align:center">All Rights Reserved @ 2018</p>
+</footer>
+</body>
+</html>
